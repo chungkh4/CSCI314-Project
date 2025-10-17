add659daf49061181f05d5054d0fe0df16c2786c
chungkh4 <chungkh4@gmail.com>
2025-10-17 13:51:25 +0800

    stuff

--- website/auth.py
+++ website/auth.py
@@ -1,11 +1,12 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 # url holders
 auth = Blueprint('auth', __name__)
 
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
+    data = request.form
     return render_template("login.html", text = "testing")
 
 
@@ -14,6 +15,6 @@ def logout():
     return "<p>.</p>"
 
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
     return render_template("sign_up.html")
