0fa138f302ed5e68ebe60384a1380aaa007532e6
chungkh4 <chungkh4@gmail.com>
2025-10-22 11:13:42 +0800

    test edit

--- website/__init__.py
+++ website/__init__.py
@@ -6,6 +6,7 @@ from flask_login import LoginManager
 db = SQLAlchemy()
 DB_NAME = "database.db"
 
+
 def create_app():
     app = Flask(__name__)
 
@@ -27,20 +28,19 @@ def create_app():
     from .models import User
     create_database(app)
 
-
     login_manager = LoginManager()
-    
+
     login_manager.login_view = 'auth.login'
     login_manager.init_app(app)
 
     @login_manager.user_loader
     def load_user(id):
         return User.query.get(int(id))
-    
 
     return app
 
-def create_database(app):   
+
+def create_database(app):
     with app.app_context():
         if not path.exists('website/' + DB_NAME):
             db.create_all()
